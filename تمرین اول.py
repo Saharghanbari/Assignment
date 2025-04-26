3ee7f7ef982be147eece23d1041065d08be0511f
Saharghanbari <sahar13880430@gmail.com>
2025-04-27 00:09:07 +0330

    Add files via upload

---
+++ تمرین اول.py
@@ -0,0 +1,6 @@
+Python 3.13.2 (tags/v3.13.2:4f8bb39, Feb  4 2025, 15:23:48) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> n = int(input(یک عدد وارد کنید))
+... 
+... for i in range(1, n + 1):
+...     for j in range(1, i + 1):
